b022287ecafe449ed5d25d4a92f27dac81a6e1e9
Anthony <tonylight2011@gmail.com>
2025-11-09 23:07:25 +0000

    3. Build a Simple Company Website with React

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+function Navbar(){
+  return (
+    <nav style={{padding:'10px', backgroundColor:'black'}}>
+      <Link to='/' style={{color:'white', marginRight:'10px'}}>Home</Link>
+      <Link to='/about' style={{color:'white', marginRight:'10px'}}>About</Link>
+      <Link to='/services' style={{color:'white', marginRight:'10px'}}>Services</Link>
+      <Link to='/contact' style={{color:'white'}}>Contact</Link>
+    </nav>
+  );
+}
+export default Navbar;
