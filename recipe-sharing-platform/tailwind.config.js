800a49248bb0516396d4014694935626f889508b
Anthony <tonylight2011@gmail.com>
2025-12-07 01:13:44 +0000

    correction

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',            // Vite's main HTML file
-    './src/**/*.{js,jsx,ts,tsx}'  // All JS/TS/JSX/TSX files in src
+    './index.html', 
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
