353309452e9c3495abf89870d525484f757c2f6a
Anthony <tonylight2011@gmail.com>
2025-12-16 18:43:45 +0000

    corrections

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,25 +1,13 @@
 import { useState } from "react";
 
 function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const { username, email, password } = formData;
 
     if (!username || !email || !password) {
       setError("All fields are required");
@@ -27,9 +15,8 @@ function RegistrationForm() {
     }
 
     setError("");
-    console.log("User Registered:", formData);
+    console.log("User Registered:", { username, email, password });
 
-    // Mock API simulation
     alert("Registration successful!");
   };
 
@@ -41,26 +28,23 @@ function RegistrationForm() {
 
       <input
         type="text"
-        name="username"
         placeholder="Username"
-        value={formData.username}
-        onChange={handleChange}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
 
       <input
         type="email"
-        name="email"
         placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <input
         type="password"
-        name="password"
         placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <button type="submit">Register</button>
