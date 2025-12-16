69a240710d4643db726ab0736e5d04f5ce495f19
Anthony <tonylight2011@gmail.com>
2025-12-16 19:51:46 +0000

    error fixing

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import PostsComponent from "./components/PostsComponent.jsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a QueryClient instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="p-4">
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="p-4">
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
